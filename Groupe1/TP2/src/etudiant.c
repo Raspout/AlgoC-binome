5a11868c6997cde7c8751ac5b43ab4e6616e237f
ReiJyun <115141511+ReiJyun@users.noreply.github.com>
2022-11-14 15:24:09 +0100

    Update etudiant.c

---
+++ Groupe1/TP2/src/etudiant.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()  
+{
+    char* tableau [5][5] ={
+    {"Prénom1", "Nom1", "Adresse1", "Note1.1", "Note1.2" },
+    {"Prénom2", "Nom2", "Adresse2", "Note2.1", "Note2.2" },
+    {"Prénom3", "Nom3", "Adresse3", "Note3.1", "Note3.2" },
+    {"Prénom4", "Nom4", "Adresse4", "Note4.1", "Note4.2" },
+    {"Prénom5", "Nom5", "Adresse5", "Note5.1", "Note5.2" }
+    };
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            printf("%s ", tableau[i][j]);
+        };
+        printf("\n");
+
+    };
+}
+

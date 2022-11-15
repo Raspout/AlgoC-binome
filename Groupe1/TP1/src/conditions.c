b24b81ae2a251ea2fb4de3e98b3eb6fb6ea91fc2
ReiJyun <115141511+ReiJyun@users.noreply.github.com>
2022-11-15 15:21:46 +0100

    Update conditions.c

--- Groupe1/TP1/src/conditions.c
+++ Groupe1/TP1/src/conditions.c
@@ -7,6 +7,7 @@ int main()
     int nombre3 = 0;
     printf("condition ET : \n");
     while (nombre1 != 1000){
+        //calcul si le nombre est divisible par 2 et 15
        if( (nombre1 % 2 == 0) && (nombre1 % 15 == 0)) {
         printf("%d\n", nombre1);
         }
@@ -14,6 +15,7 @@ int main()
     }
     printf("contion OU : \n");
     while (nombre2 != 1000){
+        //calcul si le nombre est divisible par 103 ou 107
        if( (nombre2 % 103 == 0) || (nombre2 % 107 == 0)) {
         printf("%d\n", nombre2);
         }
@@ -21,6 +23,7 @@ int main()
     }
     printf("condition OU,NON : \n");
     while (nombre3 != 1000){
+        //calcul si le nombre est divisible par 7 ou 5 mais pas par 3
         if( ((nombre3 % 7 == 0) || (nombre3 % 5 == 0)) && (nombre3 % 3 !=0)) {
         printf("%d\n", nombre3);
         }

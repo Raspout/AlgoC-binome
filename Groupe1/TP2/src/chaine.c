72072976632547f802d606fd1e21f841018ff682
Alexis Carella <62143493+Raspout@users.noreply.github.com>
2022-11-17 10:29:17 +0100

    Update chaine.c

--- Groupe1/TP2/src/chaine.c
+++ Groupe1/TP2/src/chaine.c
@@ -2,9 +2,32 @@
 
 int main()
 {
-    char str[] = "hello, I am bruno";
+    char str[] = "I am a monkey";
     int i;
     for(i = 0; str[i] > '\0'; i++);
-    printf("%d\n", i);
+    printf("Il y a %d caractères dans \"%s\"\n", i, str);
+
+    int len = i;
+
+    printf("str2 copie de str :\n");
+
+    char str2[50];
+    for (int y = 0; y <= len; y++)
+    {
+        str2[y] = str[y];
+    }
+    printf("%s\n", str2);
+
+    int j = 0;
+    printf("str et str2 concatene:\n"); 
+
+    while(str2[j]!='\0')
+    {
+      str[i]=str2[j];
+      j++;
+      i++;
+    }
+    str[i]='\0';
+    printf("%s\n",str);
     return 0;
 }

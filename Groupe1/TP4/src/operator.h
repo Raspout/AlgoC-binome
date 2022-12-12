5ad9c70bc4a5cd22f14d4ed16b68f3416dbe9122
ReiJyun <115141511+ReiJyun@users.noreply.github.com>
2022-12-12 16:08:50 +0100

    Update operator.h

--- Groupe1/TP4/src/operator.h
+++ Groupe1/TP4/src/operator.h
@@ -1,36 +1,23 @@
-#include <stdio.h>
+#ifndef h_operator
+#define h_operator
+//#include <stdio.h>
 
-int num1 = 3;
-int num2 = 4;
 
-int somme(){
-    printf("addition : %u \n", num1+num2);
-}
+int somme(int num1, int num2);
 
-int difference(){
-    printf("soustraction : %u \n", num1-num2);
-}
+int difference(int num1, int num2);
 
-int produit(){
-    printf("multiplication : %u\n", num1*num2);
-}
+int produit(int num1, int num2);
 
-int quotient(){
-    printf("quotient : %u\n", num1/num2);
-}
+int quotient(int num1, int num2);
 
-int modulo(){
-    printf("modulo : %u\n", num1%num2);
-}
+int modulo(int num1, int num2);
 
-int et(){
-    printf("ET : %u\n", num1 && num2);
-}
+int et(int num1, int num2);
 
-int ou(){
-    printf("OU : %u\n", num1 || num2);
-}
+int ou(int num1, int num2); 
 
-int negation(){
-    printf("négation : %u\n", - num1); 
-}
+int negation(int num1);
+
+
+#endif

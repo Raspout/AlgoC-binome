0f41513bc97401705d9395954f51aab4fb4cb259
ReiJyun <115141511+ReiJyun@users.noreply.github.com>
2022-12-12 16:09:15 +0100

    Update operator.c

--- Groupe1/TP4/src/operator.c
+++ Groupe1/TP4/src/operator.c
@@ -1,46 +1,35 @@
 #include <stdio.h>
 #include "operator.h"
 
-int main()
-{
-
-    char op = '&';
-    
-    switch (op)
-    {
-    case '+':
-        somme();
-        break;
-
-    case '-':
-        difference();
-        break;
-    
-    case '*':
-        produit();
-        break;
-
-    case '/':
-        quotient();
-        break;
-
-    case '%':
-        modulo();
-        break;
-
-    case '&':
-        et();
-        break;
-
-    case '|':
-        ou();
-        break;
-
-    case '~':
-        negation();
-        break;
-
-    default:
-        break;
-    }
+int somme(int num1, int num2){
+    return(num1 + num2);
 }
+
+int difference(int num1, int num2){
+    return(num1 - num2);
+}
+
+int produit(int num1, int num2){
+    return(num1 * num2);
+}
+
+int quotient(int num1, int num2){
+    return(num1 / num2);
+}
+
+int modulo(int num1, int num2){
+    return(num1 % num2);
+}
+
+int et(int num1, int num2){
+    return(num1 && num2);
+}
+
+int ou(int num1, int num2){
+    return(num1 || num2);
+}
+
+int negation(int num1){
+    return(~ (num1 - 1) ); 
+}
+

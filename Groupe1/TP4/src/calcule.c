4db2b0677f34bedd3ca906320522c5644f261bec
ReiJyun <115141511+ReiJyun@users.noreply.github.com>
2022-12-28 14:36:26 +0100

    Update calcule.c

---
+++ Groupe1/TP4/src/calcule.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include "operator.h"
+
+int main()
+{
+    char op; 
+    int num1, num2;
+   
+    printf("Calcule: ");
+    scanf("%c", &op);
+   
+    printf("Numéro1 : ");
+    scanf("%d", &num1);
+      
+    printf("Numéro2 : ");
+    scanf("%d", &num2);
+   
+    
+    switch (op)
+    {
+    case '+':
+        printf("addition : %i \n", somme(num1, num2));
+        break;
+
+    case '-':
+        printf("soustraction : %i \n", difference(num1, num2));
+        break;
+    
+    case '*':
+        printf("multiplication : %i \n", produit(num1, num2));
+        break;
+
+    case '/':
+        printf("division : %i \n", quotient(num1, num2));
+        break;
+
+    case '%':
+        printf("modulo : %i \n", modulo(num1, num2));
+        break;
+
+    case '&':
+        printf("et : %i \n", et(num1, num2));
+        break;
+
+    case '|':
+        printf("ou : %i \n", ou(num1, num2));
+        break;
+
+    case '~':
+        printf("négation : %i \n", negation(num1));
+        break;
+
+    default:
+        break;
+    }
+   
+    return 0;
+}

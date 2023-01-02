591300d23e61309b8735012daf160d8657f029de
ReiJyun <115141511+ReiJyun@users.noreply.github.com>
2023-01-02 22:56:27 +0100

    Update repertoire.c

--- Groupe1/TP5/src/repertoire.c
+++ Groupe1/TP5/src/repertoire.c
@@ -5,7 +5,7 @@
 void lire_dossier (char * chemin)
 {
     //ouverture du dossier indiqué dans chemin
-    DIR * rep = opendir (chemin); 
+    DIR *rep = opendir (chemin); 
     
     //Si rep n'est pas nul, on continue de lister
     if (rep != NULL) 
@@ -23,6 +23,37 @@ void lire_dossier (char * chemin)
         closedir (rep);
     }
     return 0;
+}
+
+void lire_dossier_recursif(char * chemin){
+    DIR *rep;
+    struct dirent *ent;
+    rep = opendir(chemin);
+    if (rep) {
+        while ((ent = readdir(rep)) != NULL) {
+
+            //Instancie le chemin
+            char chemin[200];
+            
+            //compteur dans le print
+            snprintf(chemin, sizeof(chemin), "%s/%s", nom, ent->d_name);
+            struct stat s;
+
+            // Affiche le répertoire récursivement
+            if (stat(chemin, &s) == 0 && S_ISDIR(s.st_mode)) {
+                
+                //liste le nom du repertoire et appelle la fonction lire_dossier
+                printf("%s/%s\n", nom, ent->d_name);
+                    lire_dossier_recursif(chemin);
+            }
+            else {
+                // L'élément est un fichier, on l'affiche seulement
+                printf("%s/%s\n", nom, ent->d_name);
+            }
+        }
+        //fermeture du répertoire
+        closedir(rep);
+    }
 }
 
 

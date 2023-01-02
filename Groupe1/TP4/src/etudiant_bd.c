d238f3652b1e66468877527ec6423509d053c070
ReiJyun <115141511+ReiJyun@users.noreply.github.com>
2023-01-02 23:08:06 +0100

    Update etudiant_bd.c

--- Groupe1/TP4/src/etudiant_bd.c
+++ Groupe1/TP4/src/etudiant_bd.c
@@ -5,13 +5,16 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+
 void ecrire_dans_fichier(char *nom_de_fichier, char *message){
     int fichier, compteur, taille;
+    //ouvre le fichier 
     fichier = open(nom_de_fichier, O_CREAT|O_APPEND|O_WRONLY, S_IRUSR|S_IWUSR);
     taille = write(fichier, message, sizeof(message));
     close(fichier);
 };
 
+//initialise la structure étudiant
 struct etudiant {
     char nom[50];
     char prenom[50];
@@ -23,13 +26,20 @@ struct etudiant {
 
 int main() {
     struct etudiant etudiant[5]; 
+    //boucle pour les 5 étudiants
     for(int i = 0; i < 5; i++){
         char content[50];
+        
+        //entrée de l'utilisateur
         printf("Nom: "); 
         fgets(content, sizeof(content), stdin);
+        
+        //empêche le "\n"
         int len_content;
         len_content = strlen(content);
         content[(len_content - 1)] = '\0';
+        
+        //copie l'entrée dans le contenu
         strcpy(etudiant[i].nom, strcat(content, ", "));
         strcpy(etudiant[i].nom, content);
         printf("Prénom: ");
@@ -45,10 +55,12 @@ int main() {
         fgets(content, sizeof(content), stdin);
         strcpy(etudiant[i].note_sys, content); 
 
+        //affiche les entrées
         printf("%s %s %s %s %s", etudiant[i].nom, etudiant[i].prenom, etudiant[i].adresse, etudiant[i].note_C, etudiant[i].note_sys);
 
     }
     
+    //pour chaque étudiant, le contenu est copié dans le fichier
     for(int i = 0; i < 5; i++){
         ecrire_dans_fichier("etudiant.txt", etudiant[i].nom);
         ecrire_dans_fichier("etudiant.txt", etudiant[i].prenom);

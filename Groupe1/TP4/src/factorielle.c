21bedfe387fe094f4172e3d1f0a06ab2ef05800a
ReiJyun <115141511+ReiJyun@users.noreply.github.com>
2023-01-02 23:09:40 +0100

    Update factorielle.c

--- Groupe1/TP4/src/factorielle.c
+++ Groupe1/TP4/src/factorielle.c
@@ -2,10 +2,12 @@
 
  
 int factorielle (int num) {
+    //calcule de !0
     if (num == 0) {
         return 1;
     }
     else {
+        //retourne n * (n-1) jusqu'à ce que n = 0
         return (num * factorielle (num-1));
         printf("%i", num); 
     }

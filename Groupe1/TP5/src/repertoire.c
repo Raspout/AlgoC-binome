12aed819a1aeb3242b0f326aeaf0d40ba58b2fbf
ReiJyun <115141511+ReiJyun@users.noreply.github.com>
2023-01-03 21:53:47 +0100

    Update repertoire.c

--- Groupe1/TP5/src/repertoire.c
+++ Groupe1/TP5/src/repertoire.c
@@ -56,5 +56,51 @@ void lire_dossier_recursif(char * chemin){
     }
 }
 
+void lire_dossier_iteratif(char * chemin) {
+    DIR *rep;
+    struct dirent *ent;
+    struct stat s;
+    char chemin[200];
+    const int MAX_TAILLE_PILE = 100;
+
+    // Initialisation de la pile
+    char *pile[MAX_TAILLE_PILE];
+    int indice_pile = 0;
+
+    pile[indice_pile] = chemin;
+    indice_pile++;
+
+    // Association du chemin avec la pile
+    while (indice_pile > 0) {
+        char *repertoire_courant = pile[indice_pile - 1];
+        indice_pile--;
+
+        
+        rep = opendir(repertoire_courant);
+        if (rep) {
+            
+            //Parcours le chemin tant qu'il y a du contenu 
+            while ((ent = readdir(rep)) != NULL) {
+                
+                snprintf(chemin, sizeof(chemin), "%s/%s", repertoire_courant, ent->d_name);
+                stat(chemin, &s);
+
+                // Ajout de l'élément à la pile 
+                if (S_ISDIR(s.st_mode)) {
+                        printf("%s/%s\n", chemin, ent->d_name);
+                        pile[indice_pile] = strdup(chemin);
+                        indice_pile++;
+                    }
+                }
+                else {
+                    // L'élément est un fichier, on l'affiche seulement
+                    printf("%s/%s\n", repertoire_courant, ent->d_name);
+                }
+            }
+            closedir(rep);
+        }
+    }
+}
+
 
 
